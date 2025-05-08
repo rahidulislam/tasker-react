c1f9af70756c4e7fcd0a61c384d662e0d09f380e
Rahidul Islam <rahiseli@outlook.com>
2025-05-08 08:10:43 +0600

    footer and hero section are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from './components/Footer'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 function App() {
 
   return (
     <>
       <Header />
+      <HeroSection />
+      <Footer />
     </>
   )
 }
