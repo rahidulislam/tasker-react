afcb89347b803bf72e502c886631e578f4815fc0
Rahidul Islam <rahiseli@outlook.com>
2025-05-20 11:23:00 +0600

    delete all task is done

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -45,6 +45,10 @@ export default function TaskBoard() {
     const afterDeleteTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(afterDeleteTasks);
   }
+  function handleDeleteAllClick(){
+    tasks.length = 0
+    setTasks([...tasks])
+  }
 
   return (
     <section className="mb-20" id="tasks">
@@ -61,7 +65,7 @@ export default function TaskBoard() {
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskActions onAddClick={() => setShowAddModal(true)} onDeleteAllClick={handleDeleteAllClick} />
           <TaskList
             tasks={tasks}
             onEdit={handleEditTask}
