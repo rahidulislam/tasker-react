471b5c00e952cd21227e81920a4de58d560420d9
Rahidul Islam <rahiseli@outlook.com>
2025-05-20 14:35:43 +0600

    search item is done

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -57,6 +57,13 @@ export default function TaskBoard() {
     setTasks(newTask);
   }
 
+  function handleSearch(searchTerm) {
+    const filteredTasks = tasks.filter(task=>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setTasks(filteredTasks);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddModal && (
@@ -68,7 +75,7 @@ export default function TaskBoard() {
       )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask />
+          <SearchTask onSearch={handleSearch}/>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
