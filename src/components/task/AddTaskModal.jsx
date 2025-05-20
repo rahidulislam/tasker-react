78eaf2fe106b64c0e68d6bab8534d0580a82bc7d
Rahidul Islam <rahiseli@outlook.com>
2025-05-20 09:19:44 +0600

    edit task is done and close buttion is done for add and edit task

--- src/components/task/AddTaskModal.jsx
+++ src/components/task/AddTaskModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-export default function AddTaskModal({onSave}) {
-  const [task, setTask] = useState({
+export default function AddTaskModal({onSave, taskToUpdate, onCloseClick}) {
+  const [task, setTask] = useState(taskToUpdate || {
     id: crypto.randomUUID(),
     title: "",
     description: "",
@@ -8,6 +8,7 @@ export default function AddTaskModal({onSave}) {
     priority: "",
     isFavorite: false,
   });
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
   function handleChange(e) {
     const name = e.target.name;
     let value = e.target.value;
@@ -24,7 +25,7 @@ export default function AddTaskModal({onSave}) {
       <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
       <htmlForm className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          { isAdd ? "Add New Task" : "Edit Task" }
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -87,11 +88,18 @@ export default function AddTaskModal({onSave}) {
           </div>
         </div>
 
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between lg:mt-20">
+          <button
+            
+            className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={onCloseClick}
+          >
+            close
+          </button>
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-            onClick={()=>{onSave(task);}}
+            onClick={()=>{onSave(task, isAdd);}}
           >
             Save
           </button>
